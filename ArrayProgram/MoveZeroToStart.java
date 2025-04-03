92b34b013c1b9d9f7fcaf6388c4da419f8997bc4
imrajeevnayan <immrrobot121@gmail.com>
2025-04-03 09:31:29 +0530

    some changes

--- ArrayProgram/MoveZeroToStart.java
+++ ArrayProgram/MoveZeroToStart.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-
 public class MoveZeroToStart {
     public static void MoveZeroStart(int []arr){
         int j=arr.length-1;
@@ -14,9 +13,8 @@ public class MoveZeroToStart {
         }
     }
     public static void main(String[] args) {
-        int[]arr={0,1,2,4,6,8,0,2,4,5,0,9};
+        int[]arr={0,1,2,4,6,8,0,2,4,5,0,0,0,9,11,12};
         MoveZeroStart(arr);
-        //System.out.println(arr.toString());
         for(int i=0;i<arr.length;i++){
             Arrays.sort(arr);
             System.out.print(arr[i]+" ");

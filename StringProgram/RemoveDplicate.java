b12634b8d901a31a870d559594517a321949885b
imrajeevnayan <immrrobot121@gmail.com>
2025-04-07 21:45:07 +0530

    #

--- StringProgram/RemoveDplicate.java
+++ StringProgram/RemoveDplicate.java
@@ -1,18 +1,14 @@
 public class RemoveDplicate {
     public static String removeDuplicate(String s){
         String result="";
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(!result.contains(String .valueOf(ch))){
-                result+=ch;
-            }
-
+        for(char c:s.toLowerCase().toCharArray()){
+            if(result.indexOf(c)==-1) result+=c;
         }
         return result;
 
     }
     public static void main(String[] args) {
-        String input="JAVA PROGRAMMING";
+        String input="JAVA PROGRAMmING";
         System.out.println(removeDuplicate(input));
     }
     
